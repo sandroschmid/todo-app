a77ebbd6eaeae65721844d8e6710908e2ae7807c
Sandro Schmid <sandro.schmid@dynatrace.com>
2020-05-23 18:40:28 +0200

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+import { FIREBASE_CONFIG } from './firebase-config';
+
 export const environment = {
   production: true,
+  firebase: FIREBASE_CONFIG,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { FIREBASE_CONFIG } from './firebase-config';
+
 export const environment = {
   production: false,
+  firebase: FIREBASE_CONFIG,
 };
 
 /*
